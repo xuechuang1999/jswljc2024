bed0d02329d1915434a6d46646290b6b3cd5fe1c
xuechuang1999 <2321.52461@qq.com>
2024-03-06 17:10:17 +0800

    Chinese

--- homework1.c
+++ homework1.c
@@ -11,27 +11,27 @@ int main()
     int final_engscore[5]={0,0,0,0,0};
     for (int i =0; i<5; i++)
     {
-        printf("Please enter the %d student ID: ", i+1);
+        printf("请输入第%d位学生的ID: ", i+1);
         scanf("%8s", &id[i]);
-        printf("Enter the score of student %s in Computer Science and  English: ", &id[i]);
+        printf("请输入%s同学的计算物理和英语分数: ", &id[i]);
         scanf("%d %d", &cpscore[i], &engscore[i]);
         final_cpscore[i] = sqrt(cpscore[i]) * 10;
         final_engscore[i] = sqrt(engscore[i]) * 10;
         if (final_cpscore[i] < 60)
         {
-            printf("Student %d failed in Computer Science\n", &id[i]);
+            printf("%s同学的计算物理成绩不及格\n", &id[i]);
         }
         else
         {
-            printf("Student %d passed in Computer Science\n", &id[i]);
+            printf("%s同学通过了计算物理考试\n", &id[i]);
         }
         if (final_engscore[i] < 60)
         {
-            printf("Student %d failed in English\n", &id[i]);
+            printf("%s同学的英语成绩不及格\n", &id[i]);
         }
         else
         {
-            printf("Student %d passed in Eglish\n", &id[i]);
+            printf("%s同学通过了英语考试\n", &id[i]);
         }
     }
     return 0;
